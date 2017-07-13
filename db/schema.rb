13d1513ff0231e73ed09d56613895a448c022df2
Rails Udemy Course <cfisher440@gmail.com>
2017-07-13 14:30:01 +0000

    fixed friendship model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170713135011) do
+ActiveRecord::Schema.define(version: 20170713142705) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer  "user_id"
